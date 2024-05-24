787eb468ae3e7983ec7a24ed017dc9dcdbc7c11a
Subin Lee <ewaterbinn@gmail.com>
2024-05-24 15:45:10 +0900

    style: 글로벌 스타일, 모듈 prepend 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,31 +1,46 @@
-import type { StorybookConfig } from '@storybook/nextjs';
 const path = require('path');
 
-const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+module.exports = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        sass: {          
+          implementation: require("sass"),
+          prependData: `@import "@/styles/common/_variables.scss";`
+        }
+    }
+  }
   ],
+
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {
+      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+    },
   },
-  docs: {
-    autodocs: 'tag',
-  },
-  staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '../src'),
-      };
-    }
+
+  webpackFinal: (config) => {
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '@': [path.resolve(__dirname, '../src/'), path.resolve(__dirname, '../')],
+    };
+    config.resolve.roots = [
+      path.resolve(__dirname, '../public'),
+      'node_modules',
+    ];
     return config;
   },
+
+  docs: {  
+    autodocs: 'tag',
+  },
 };
-export default config;
